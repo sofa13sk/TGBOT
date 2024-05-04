481e128191ea1ed3ee330069aa22fa48c2bb6ff6
sofa13sk <166922231+sofa13sk@users.noreply.github.com>
2024-05-04 16:26:57 +0200

    Delete kb.py

--- kb.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-kb_1 = InlineKeyboardMarkup(row_width=1)
-
-b_1 = InlineKeyboardButton(text="ПОЛУЧИ РЕЦЕПТ", url="https://www.russianfood.com/recipes/bytype/?fid=643&ysclid=lvi4ea80st184033379")
-b_2 = InlineKeyboardButton(text="ПОЛУЧИТЬ СЛУЧАЙНОЕ ЧИСЛО", callback_data="☻")
-b_3 = InlineKeyboardButton(text="ДРУГАЯ КНОПКА", callback_data="fgh")
-kb_1.add(b_1, b_2, b_3)
